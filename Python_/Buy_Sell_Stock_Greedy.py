862ec40b3640d840543b35e8bda7f2bb60a3da05
Harshita Singh <30619470+HarshitaSing@users.noreply.github.com>
2023-06-21 00:55:01 -0700

    Best Time to Buy and Sell Stock

---
+++ Python_/Buy_Sell_Stock_Greedy.py
@@ -0,0 +1,18 @@
+#greedy_algorithm
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        left_ptr = 0 
+        right_ptr = 1
+        max_profit = 0
+        while right_ptr < len(prices):
+            curr_profit = prices[right_ptr] - prices[left_ptr] #to get profit for each day
+            if prices[left_ptr] < prices[right_ptr]:
+                max_profit = max(curr_profit, max_profit)
+            else:
+                left_ptr = right_ptr
+            right_ptr += 1
+        
+        return max_profit
+
+                
